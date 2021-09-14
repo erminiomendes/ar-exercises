2ff9b7960a719f1a00c6b6beb91078a23552e343
Erminio <Erminio>
2021-09-14 16:56:43 +0000

    Ruby Ex. 01, 02 and 03

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.where(id:1)
+@store2 = Store.where(id:2)
+
+store = Store.find_by(id: 1)
+store.update(name: 'Vancouver')
+
+
